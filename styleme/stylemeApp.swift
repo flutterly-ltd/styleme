7983d1dbbcbe4e9942998ef17d8ee59da4bff285
Anoop Jose <anoop@flutterly.co.uk>
2021-09-03 14:17:12 +0100

    Initial Commit

---
+++ styleme/stylemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  stylemeApp.swift
+//  styleme
+//
+//  Created by Anoop Jose on 03/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct stylemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
